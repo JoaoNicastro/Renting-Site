56881369bad3f26df360107ff62bde332815917c
christianhulot <122960708+christianhulot@users.noreply.github.com>
2024-02-20 05:43:48 +0000

    ada

--- Code_site/Clients.py
+++ Code_site/Clients.py
@@ -1,7 +1,9 @@
+
 from dotenv import load_dotenv
 import os
 import psycopg2
 import sys
+import time
 
 print(sys.path)
 
@@ -16,7 +18,13 @@ DB_PORT = "5432"  # Port exposed by PostgreSQL container
 
 def get_users():
     try:
+        time.sleep(10)
         # Attempt to establish a database connection
+        print(DB_HOST)
+        print(DB_NAME)
+        print(DB_PASSWORD) 
+        print(DB_PORT)
+        print(DB_USER)
         conn = psycopg2.connect(
             dbname=DB_NAME,
             user=DB_USER,
@@ -41,6 +49,22 @@ def get_users():
 
     except psycopg2.Error as connection_error:
         print(f"Database connection failed: {connection_error}")
-
-get_users()
 #docker inspect code_site_db_1
+
+"""
+import json
+
+# Path to the JSON file written by Node.js
+file_path = '/workspaces/Renting-Site/Code_site/clientsData.json'
+
+# Reading the JSON data from the file
+try:
+    with open(file_path, 'r') as file:
+        clients_data = json.load(file)
+        for client in clients_data:
+            print(client)  # Process each client's data as needed
+except FileNotFoundError:
+    print(f"The file {file_path} does not exist.")
+except json.JSONDecodeError:
+    print(f"Error decoding JSON from the file {file_path}.")
+"""
--- Code_site/index.js
+++ Code_site/index.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcrypt'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
+const cron = require('node-cron');
+const fs = require('fs');
 
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
@@ -147,7 +149,6 @@ module.exports = app;
 
 // Other routes remain unchanged...
 
-
 // GET /login
 app.get('/login', (req, res) => {
   res.render('pages/login', { user: null }); // Pass user as null
@@ -225,5 +226,6 @@ app.get('/profile', async (req, res) => {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
+
 app.listen(3000);
 console.log('Server is listening on port 3000');
